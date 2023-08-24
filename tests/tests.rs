3e421296cd22743de47cfe14f03f70c590751ebc
Albert·Gou <ggq89@qq.com>
2023-08-25 00:19:57 +0800

    add read_files_in_directory

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,25 @@
 use serde_derive::{Deserialize, Serialize};
+use std::fs;
 use std::fs::File;
 use std::io::Read;
+
+fn read_files_in_directory(directory: &str) -> Result<Vec<String>, std::io::Error> {
+    let mut file_contents = Vec::new();
+
+    for entry in fs::read_dir(directory)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            let mut file = File::open(&path)?;
+            let mut content = String::new();
+            file.read_to_string(&mut content)?;
+            file_contents.push(content);
+        }
+    }
+
+    Ok(file_contents)
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct SignTestCase {
@@ -14,15 +33,6 @@ struct SignTestInput {
     message: String,
 }
 
-fn read_json_file(file_path: &str) -> Result<SignTestCase, Box<dyn std::error::Error>> {
-    let mut file = File::open(file_path)?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
-
-    let parsed_data: SignTestCase = serde_json::from_str(&content)?;
-
-    Ok(parsed_data)
-}
 
 
 #[cfg(test)]
