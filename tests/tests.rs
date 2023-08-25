c3b1f8b57b587ae76aad04fe245a412ce6aa06a7
Albert·Gou <ggq89@qq.com>
2023-08-25 11:10:01 +0800

    add test_sign

--- tests/tests.rs
+++ tests/tests.rs
@@ -3,6 +3,7 @@ use std::fs;
 use std::fs::File;
 use std::io::Read;
 
+
 fn read_files_in_directory(directory: &str) -> Result<Vec<String>, std::io::Error> {
     let mut file_contents = Vec::new();
 
@@ -33,27 +34,53 @@ struct SignTestInput {
     message: String,
 }
 
-fn read_sign_test_cases() -> Result<Vec<SignTestCase>, Box<dyn std::error::Error>> {
-    let file_contents = read_files_in_directory("test_cases/sign")?;
+fn read_sign_test_cases() -> Vec<SignTestCase> {
+    let file_contents = read_files_in_directory("test_cases/sign")
+        .unwrap_or_else(|err| panic!("Error reading test cases: {:?}", err));
+
     let mut test_cases = Vec::new();
 
     for content in file_contents {
-        let test_case = serde_json::from_str(&content)?;
+        let test_case = serde_json::from_str(&content)
+            .unwrap_or_else(|err| panic!("Error parsing test case: {:?}", err));
+        
         test_cases.push(test_case);
     }
 
-    Ok(test_cases)
+    test_cases
 }
 
 
 #[cfg(test)]
 mod tests {
+    use ark_serialize::Compress;
+    use bls_verify_gadget::bls::{PrivateKey, Parameters, BLS};
+
     use super::*;
 
     #[test]
     fn test_sign() {
+        let test_cases = read_sign_test_cases();
+
+        for (_, test_case) in test_cases.iter().enumerate() {    
+            let private_bytes = hex::decode(&test_case.input.privkey[2..]).unwrap();
+            let message_bytes = hex::decode(&test_case.input.message[2..]).unwrap();
+            private_bytes.reverse();
+            let private_key = PrivateKey::from(&private_bytes[..]);
+            
+            let parameters = Parameters::default();
+            let mut rng = ark_std::test_rng();
+
+            let signature = BLS::sign(&parameters, &private_key, &message_bytes, &mut rng).unwrap();
+ 
+            let mut serialized = vec![0u8; 0];
+            let mut size = 0;
+            size += sig.serialized_size(Compress::Yes);
         
-        assert_eq!(2 + 2, 4);
+            serialized.resize(size, 0u8);
+            sig.serialize_compressed(&mut serialized[..]).unwrap();
+            assert_eq!(test_case.output, hex.encode(serialized));          
+        }
     }
 }
 
