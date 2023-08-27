3dc39458a64d0812b3a7532911651446d7ca2b39
keep <readygo67@github.com>
2023-08-27 17:05:04 +0800

    add From/Into method for privatekey,publickey,signature

--- src/bls.rs
+++ src/bls.rs
@@ -2,6 +2,7 @@ use ark_crypto_primitives::signature::SignatureScheme;
 use ark_crypto_primitives::Error;
 use ark_bls12_381::{Bls12_381, Fr, G1Projective, G2Projective, G1Affine};
 
+use ark_ff::{BigInt};
 use ark_serialize::{CanonicalSerialize, CanonicalDeserialize};
 use ark_ec::pairing::{Pairing, PairingOutput};
 use ark_ec::Group;
@@ -13,17 +14,25 @@ use ark_std::{rand::Rng, ops::Mul, ops::Neg, UniformRand, One};
 
 use std::borrow::Borrow;
 use sha2::Sha256;
+use hex;
 
 pub use ark_ec::pairing::*;
 
-#[derive(Default)]
 #[derive(Clone, Debug, CanonicalSerialize, CanonicalDeserialize)]
 pub struct Parameters {
     pub g1_generator : G1Projective,
 }
 
+impl Default for Parameters {
+    fn default() -> Self {
+        Parameters {
+            g1_generator : G1Projective::generator(),
+        }
+    }
+}
+
 #[derive(Default)]
-#[derive(Clone, Debug, CanonicalSerialize, CanonicalDeserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, CanonicalSerialize, CanonicalDeserialize)]
 pub struct PrivateKey(Fr);
 
 impl From<Fr> for PrivateKey {
@@ -32,6 +41,50 @@ impl From<Fr> for PrivateKey {
     }
 }
 
+//
+impl From<String> for PrivateKey {
+    fn from(s: String) -> PrivateKey {  
+        let bytes: Vec<u8> = hex::decode(s).unwrap();
+        PrivateKey::deserialize_compressed(&bytes[..]).unwrap()    
+    }
+}
+
+impl From<&str> for PrivateKey {
+    fn from(s: &str) -> PrivateKey {
+        let bytes = hex::decode(s).unwrap();
+        PrivateKey::deserialize_compressed(&bytes[..]).unwrap()    
+    }
+}
+
+impl From<&[u8]> for PrivateKey {
+    fn from(bytes: &[u8]) -> PrivateKey {
+        PrivateKey::deserialize_compressed(&bytes[..]).unwrap()  
+    }
+}
+
+//[u64;4] must be MontBackend representation
+impl From<[u64;4]> for PrivateKey {
+    fn from(sk:[u64;4]) -> PrivateKey {
+        let val = BigInt(sk);
+        PrivateKey(Fr::from(val))
+    }
+}
+
+impl Into<String> for PrivateKey{
+    fn into(self) -> String {
+        let mut serialized = vec![0; 32];
+        self.serialize_compressed(&mut serialized[..]).unwrap();
+        hex::encode(serialized)
+    }
+}
+
+impl Into<Vec<u8>> for PrivateKey{
+    fn into(self) -> Vec<u8> {
+        let mut serialized = vec![0; 32];
+        self.serialize_compressed(&mut serialized[..]).unwrap();
+        serialized
+    }
+}
 impl AsRef<Fr> for PrivateKey {
     fn as_ref(&self) -> &Fr {
         &self.0
@@ -39,7 +92,7 @@ impl AsRef<Fr> for PrivateKey {
 }
 
 #[derive(Default)]
-#[derive(Clone, Eq, Debug, PartialEq, Hash, CanonicalSerialize, CanonicalDeserialize)]
+#[derive(Clone, Copy, Eq, Debug, PartialEq, Hash, CanonicalSerialize, CanonicalDeserialize)]
 pub struct PublicKey(G1Projective);
 
 impl From<G1Projective> for PublicKey {
@@ -61,6 +114,36 @@ impl From<&PrivateKey> for PublicKey {
     }
 }
 
+impl From<String> for PublicKey {
+    fn from(s:String) -> PublicKey {
+        let bytes: Vec<u8> = hex::decode(s).unwrap();
+        PublicKey::deserialize_compressed(&bytes[..]).unwrap()    
+    }
+}
+
+impl From<&str> for PublicKey {
+    fn from(s:&str) -> PublicKey {
+        let bytes = hex::decode(s).unwrap();
+        PublicKey::deserialize_compressed(&bytes[..]).unwrap()    
+    }
+}
+
+impl Into<String> for PublicKey{
+    fn into(self) -> String {
+        let mut serialized = vec![0; 48];
+        self.serialize_compressed(&mut serialized[..]).unwrap();
+        hex::encode(serialized)
+    }
+}
+
+impl Into<Vec<u8>> for PublicKey{
+    fn into(self) -> Vec<u8> {
+        let mut serialized = vec![0;48];
+        self.serialize_compressed(&mut serialized[..]).unwrap();
+        serialized
+    }
+}
+
 impl PublicKey {
     pub fn aggregate<P: Borrow<PublicKey>>(public_keys: impl IntoIterator<Item = P>) -> PublicKey {
         public_keys.into_iter()
@@ -71,7 +154,7 @@ impl PublicKey {
 }
 
 #[derive(Default)]
-#[derive(Clone, Debug, PartialEq, Eq, CanonicalSerialize, CanonicalDeserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, CanonicalSerialize, CanonicalDeserialize)]
 pub struct Signature(G2Projective);
 
 impl From<G2Projective> for Signature {
@@ -83,6 +166,37 @@ impl From<G2Projective> for Signature {
 impl AsRef<G2Projective> for Signature {
     fn as_ref(&self) -> &G2Projective {
         &self.0
+    }
+}
+
+impl From<String> for Signature {
+    fn from(s:String) -> Signature {
+        let bytes = hex::decode(s).unwrap();
+        Signature::deserialize_compressed(&bytes[..]).unwrap()    
+    }
+}
+
+impl From<&str> for Signature {
+    fn from(s:&str) -> Signature {
+        let bytes = hex::decode(s).unwrap();
+        Signature::deserialize_compressed(&bytes[..]).unwrap()    
+    }
+}
+
+
+impl Into<String> for Signature{
+    fn into(self) -> String {
+        let mut serialized = vec![0; 96];
+        self.serialize_compressed(&mut serialized[..]).unwrap();
+        hex::encode(serialized)
+    }
+}
+
+impl Into<Vec<u8>> for Signature{
+    fn into(self) -> Vec<u8> {
+        let mut serialized = vec![0;96];
+        self.serialize_compressed(&mut serialized[..]).unwrap();
+        serialized
     }
 }
 
